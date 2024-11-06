2309b0272e7874544b0d0ef42c1337412044c0d6
Abhay Singh <246abhaysingh@gmail.com>
2024-11-05 21:39:07 -0800

    finished about scaling based on screen width

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 export default function About() {
     return (
-        <div className="flex flex-col items-center max-w-screen-lg mx-auto">
-            <h1 className="text-white text-4xl font-ubuntu font-bold pt-28 mb-10">About Me</h1>
-            <div className="w-[400px] h-[1px] bg-gray-300"></div>
+        <motion.div
+            className="flex flex-col items-center max-w-screen-lg mx-auto"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.25 }}
+            viewport={{ once: true, amount: 0.25 }}
+        >
+            <h1 className="text-white text-5xl font-ubuntu font-bold pt-28 mb-10">About Me</h1>
+            <div className="w-[400px] h-[2px] bg-gray-300"></div>
             <div className="bg-gray-800 text-gray-100 p-6 rounded-sm mx-auto w-[500px] mt-28 xl:w-[800px] lg:w-[800px] md:w-[700px] border border-customBlue">
                 <p className="text-xl">
                     I am currently a student pursuing a B.S. in Computer Science and Minor in Informatics
@@ -14,11 +21,11 @@ export default function About() {
                     to problems.
                 </p>
                 <p className="text-xl mt-6">
-                    Outside of coding; I love to play games, workout, try new foods, and
+                    Outside of my career; I love to play games, workout, try new foods, and
                     pick up new skills. I love to play basketball, football, golf, table tennis, and
                     tennis. I enjoy meeting new people and finding new hobbies.
                 </p>
             </div>
-        </div>
+        </motion.div>
     );
 }   
